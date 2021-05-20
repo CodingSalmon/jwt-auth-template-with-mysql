88b4419aee8f65b052f73e2c4d0a65628a4a139d
Samuel Gemberling <sgembeling@gmail.com>
2021-05-19 21:38:26 -0500

    add routing auth

--- routes/auth.js
+++ routes/auth.js
@@ -11,9 +11,14 @@ router.get('/users', authCtrl.getUsers)
 router.get('/friends', authCtrl.getFriendRequests)
 
 router.use(require('../config/auth'));
-router.get('/friends/:userId', authCtrl.getFriends)
-router.post('/add-friend/:id1/:id2', authCtrl.addFriend)
-router.put('/accept-friend/:id1/:id2', authCtrl.acceptFriend)
-router.delete('/delete-friend/:id1/:id2', authCtrl.deleteFriend)
+router.get('/friends/:userId', checkAuth, authCtrl.getFriends)
+router.post('/add-friend/:id1/:id2', checkAuth, authCtrl.addFriend)
+router.put('/accept-friend/:id1/:id2', checkAuth, authCtrl.acceptFriend)
+router.delete('/delete-friend/:id1/:id2', checkAuth, authCtrl.deleteFriend)
+
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({msg: 'Not Authorized'});
+}
 
 module.exports = router;
